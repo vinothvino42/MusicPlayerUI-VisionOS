7e7d14b6ebc636036c3e1b3094619c4e9872196e
vinothvino42 <vinothvino42@gmail.com>
2023-07-22 14:20:51 +0530

    chore: add music player app boilerplate

---
+++ MusicPlayerUI-VisionOS/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  MusicPlayerUI-VisionOS
+//
+//  Created by Vinoth Vino on 22/07/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicPlayerUI-VisionOS/MusicPlayerUI_VisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerUI_VisionOSApp.swift
+//  MusicPlayerUI-VisionOS
+//
+//  Created by Vinoth Vino on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerUI_VisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
